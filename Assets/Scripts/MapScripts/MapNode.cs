a1d825e455930ca92f0a563e671013bc3eddbbeb
Ahmet Bera Hasanoğlu <ahasanoglu0@gmail.com>
2026-02-03 14:54:57 +0300

    harita

--- Assets/Scripts/MapScripts/MapNode.cs
+++ Assets/Scripts/MapScripts/MapNode.cs
@@ -7,15 +7,14 @@ public class MapNode : MonoBehaviour
     [Header("Ayarlar")]
     public NodeType nodeType;
     
-    // NERELERE GİDİLEBİLİR? (Bunu elle dolduracaksın)
     public List<MapNode> outgoingPaths = new List<MapNode>(); 
 
     [Header("Görsel")]
     public Button nodeButton;
     public Image iconImage;
-public Image contentImage; // Butonun içindeki İKON resmi (Çerçeve değil)
+//public Image contentImage; // Butonun içindeki İKON resmi (Çerçeve değil)
 
-    public void Setup(NodeType type, int layer, MapConfig config) // Config parametresi ekledik
+   /* public void Setup(NodeType type, int layer, MapConfig config) // Config parametresi ekledik
     {
         nodeType = type;
         
@@ -25,7 +24,7 @@ public Image contentImage; // Butonun içindeki İKON resmi (Çerçeve değil)
             contentImage.sprite = config.GetIcon(type);
             contentImage.preserveAspect = true; // Resim sünmesin diye
         }
-    }
+    }*/
     void Start()
     {
         // Buton tıklamasını dinle
@@ -46,22 +45,17 @@ public Image contentImage; // Butonun içindeki İKON resmi (Çerçeve değil)
         }
     }
 
-    // --- EDİTÖRDE ÇİZGİLERİ GÖRMEK İÇİN SİHİRLİ KOD ---
-    // Bu kod oyun çalışırken değil, sen editörde tasarım yaparken çalışır.
     void OnDrawGizmos()
     {
         if (outgoingPaths == null || outgoingPaths.Count == 0) return;
 
-        Gizmos.color = Color.yellow; // Çizgi rengi
+        Gizmos.color = Color.yellow; 
 
         foreach (var node in outgoingPaths)
         {
             if (node != null)
             {
-                // Mevcut noktadan, hedef noktaya sarı bir çizgi çek
                 Gizmos.DrawLine(transform.position, node.transform.position);
-                
-                // Ok ucu gibi bir küre koy (Yönü belli olsun)
                 Vector3 direction = (node.transform.position - transform.position).normalized;
                 Gizmos.DrawSphere(Vector3.Lerp(transform.position, node.transform.position, 0.2f), 10f);
             }

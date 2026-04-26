393a953ab368319264c8dd8b202f575b674d8fc7
Ahmet Bera Hasanoğlu <ahasanoglu0@gmail.com>
2026-04-27 01:51:18 +0300

    Itemsets

--- Assets/ScriptableObjects/ItemData.cs
+++ Assets/ScriptableObjects/ItemData.cs
@@ -8,15 +8,30 @@ public enum ItemType
     Helmet
 }
 
+
+public enum ItemSetType
+{
+    None,      
+    Fire,       // Ateş  (Yakma)
+    Poison,     // Zehir (Zamanla Hasar)
+    Faith       // İnanç (Şifa / Kutsal Alan)
+}
+
 [CreateAssetMenu(menuName = "Data/Item")]
 public class ItemData : ScriptableObject
 {
     public string itemID;
     public ItemType type;
+    
+    [Header("Set ve Sinerji Ayarları (YENİ)")]
+    [Tooltip("Bu eşyanın hangi efsanevi sete ait olduğunu belirler.")]
+    public ItemSetType setType = ItemSetType.None; 
+
+    [Header("Görsel ve Fiyat")]
     public Sprite icon;
     public int price;
 
-    [Header("Skinned Mesh Ayarı (YENİ)")]
+    [Header("Skinned Mesh Ayarı")]
     [Tooltip("Karakterin içindeki açılacak olan mesh objesinin tam adı (Örn: 'Sword_Iron')")]
     public string targetMeshName; 
 
@@ -25,6 +40,6 @@ public class ItemData : ScriptableObject
     public int bonusDefense;
     public int bonusSpeed;
     public int bonusStamina;
-    public float weaponRange = 2.0f; // Kılıç  2 Mızrak  3.5 Yay 10 
+    public float weaponRange = 2.0f; // Kılıç 2, Mızrak 3.5, Yay 10 
     public bool isRanged = false; 
 }

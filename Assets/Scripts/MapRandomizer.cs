5c57a14bfcecff9d760c251b3da5d771e7b9ec99
Ahmet Bera Hasanoğlu <ahasanoglu0@gmail.com>
2026-05-05 00:39:57 +0300

    maprandomizer

--- Assets/Scripts/MapRandomizer.cs
+++ Assets/Scripts/MapRandomizer.cs
@@ -7,36 +7,49 @@ public class MapRandomizer : MonoBehaviour
     public static MapRandomizer Instance;
 
     [Header("Tier Havuzları (Event Dağılımı)")]
-    public List<NodeType> tier1Pool = new List<NodeType> { NodeType.Village, NodeType.RestArea, NodeType.NasibEncounter, NodeType.DervishEncounter, NodeType.FirstBattle };
-    public List<NodeType> tier2Pool = new List<NodeType> { NodeType.Battle, NodeType.Tuccar, NodeType.Atyarisi, NodeType.YagliGures, NodeType.Dice, NodeType.CaravanEncounter };
-    public List<NodeType> tier3Pool = new List<NodeType> { NodeType.Zindan, NodeType.KuleSavas, NodeType.Treasure, NodeType.Kalkan, NodeType.Vahsi, NodeType.Archery };
+    // TIER 1: Güvenli başlangıç bölgesi. Kervansaray yok, çünkü daha çanta boş.
+    public List<NodeType> tier1Pool = new List<NodeType> { 
+        NodeType.Village, NodeType.RestArea, NodeType.NasibEncounter, 
+        NodeType.DervishEncounter, NodeType.FirstBattle 
+    };
+    
+    // TIER 2: Orta zorluk. Riskler artıyor ama Kervansaray çıkma şansı var!
+    public List<NodeType> tier2Pool = new List<NodeType> { 
+        NodeType.Battle, NodeType.Tuccar, NodeType.Atyarisi, 
+        NodeType.YagliGures, NodeType.Dice, NodeType.CaravanEncounter, 
+        NodeType.KervanEncounter // <-- Buraya eklendi
+    };
+    
+    // TIER 3: Cehennem. Bol kazançlı ama çok tehlikeli. Çantayı kurtarmak için Kervansaray çok değerli!
+    public List<NodeType> tier3Pool = new List<NodeType> { 
+        NodeType.Zindan, NodeType.KuleSavas, NodeType.Treasure, 
+        NodeType.Kalkan, NodeType.Vahsi, NodeType.Archery, 
+        NodeType.KervanEncounter // <-- Buraya eklendi
+    };
 
     void Awake()
     {
         Instance = this;
     }
 
+    // Sürgün yediğimizde veya yeni sefere başlarken RunManager burayı çağırır
     public void RandomizeMap()
     {
-        // 1. Tüm Node'ları bul
         var allNodes = FindObjectsByType<MapNode>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         if (allNodes.Length == 0) return;
 
-        // 2. Haritanın genişliğini hesapla (X koordinatına göre)
         float minX = allNodes.Min(n => n.GetComponent<RectTransform>().anchoredPosition.x);
         float maxX = allNodes.Max(n => n.GetComponent<RectTransform>().anchoredPosition.x);
-        float tierWidth = (maxX - minX) / 3f; // Haritayı 3 dikey dilime bölüyoruz
+        float tierWidth = (maxX - minX) / 3f; 
 
-        // 3. Node'ları Tier'lara göre rastgeleleştir
         foreach (var node in allNodes)
         {
-            // Başlangıç ve Boss noktalarının yeri ve türü sabittir, onlara dokunma!
             if (node.nodeType == NodeType.StartPoint || node.nodeType == NodeType.Boss) continue;
 
             float xPos = node.GetComponent<RectTransform>().anchoredPosition.x;
             NodeType newType;
 
-            // X pozisyonuna göre hangi Tier'da olduğunu bul ve o havuzdan rastgele bir event seç
+            // X pozisyonuna göre hangi Tier'da olduğunu bul
             if (xPos < minX + tierWidth) 
                 newType = tier1Pool[Random.Range(0, tier1Pool.Count)];
             else if (xPos < minX + (tierWidth * 2)) 
@@ -44,15 +57,13 @@ public class MapRandomizer : MonoBehaviour
             else 
                 newType = tier3Pool[Random.Range(0, tier3Pool.Count)];
 
-            // Node'u güncelle
             node.nodeType = newType;
             node.iconImage.sprite = GetSpriteForEvent(newType);
         }
 
-        Debug.Log("Harita Rastgeleleştirildi! Yeni bir sefer (Run) başlıyor.");
+        Debug.Log("Harita Tierlara Göre Rastgele Dağıtıldı!");
     }
 
-    // MapEventManager'daki görselleri otomatik çeker
     private Sprite GetSpriteForEvent(NodeType type)
     {
         MapEventManager em = MapEventManager.Instance;
@@ -74,10 +85,11 @@ public class MapRandomizer : MonoBehaviour
             case NodeType.Vahsi: return em.wildSprite;
             case NodeType.Dice: return em.diceSprite;
             case NodeType.YagliGures: return em.wrestlingSprite;
-            case NodeType.RestArea: return em.villageSprite; // Dinlenme için özel sprite ekleyebilirsin
-            case NodeType.NasibEncounter: return em.villageSprite; // Derviş için özel sprite ekleyebilirsin
+            case NodeType.RestArea: return em.villageSprite; 
+            case NodeType.NasibEncounter: return em.villageSprite; 
             case NodeType.DervishEncounter: return em.villageSprite; 
             case NodeType.CaravanEncounter: return em.merchant;
+            case NodeType.KervanEncounter: return em.merchant; 
             default: return em.villageSprite; 
         }
     }
